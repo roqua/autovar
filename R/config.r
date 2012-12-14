a14db2c8c92d727975702c27c76ccc92fcd35f11
Ando Emerencia <ando.emerencia@gmail.com>
2012-12-14 20:05:11 +0100

    print(av_state) now reports missing values

--- R/config.r
+++ R/config.r
@@ -14,12 +14,14 @@ print.av_state <- function(x,...) {
   for (i in seq_len(ll)) {
     cat(nn[i],": ",sep="")
     if (nn[i] == 'raw_data') {
-      cat(paste(dim(x[[i]])[1],'samples with',dim(x[[i]])[2],'features'),"\n")
+      cat(paste(dim(x[[i]])[1],'samples with',dim(x[[i]])[2],'features'),
+          " [missing: ",calc_missing(x[[i]]),"]\n",sep="")
     } else if (nn[i] == 'data') {
       if (class(x[[i]]) == 'list') {
         cat("\n")
         for (j in names(x[[i]])) {
-         cat("  ",j,": ",paste(dim(x[[i]][[j]])[1],'samples with',dim(x[[i]][[j]])[2],'features'),"\n",sep="")
+         cat("  ",j,": ",paste(dim(x[[i]][[j]])[1],'samples with',dim(x[[i]][[j]])[2],'features'),
+             " [missing: ",calc_missing(x[[i]][[j]]),"]\n",sep="")
         }
       } else {
         cat(paste(dim(x[[i]])[1],'samples with',dim(x[[i]])[2],'features'),"\n")
