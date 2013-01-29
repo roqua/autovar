fb866c79c838d531dba778a96b146c3acb06162e
Ando Emerencia <ando.emerencia@gmail.com>
2013-01-29 15:20:59 +0100

    Implemented tests for sort_models.

--- inst/tests/test_sort_models.r
+++ inst/tests/test_sort_models.r
@@ -1,7 +1,12 @@
 context('sort_models')
 
 test_that('models are sorted correctly',{
-  reset_state()
-  
-  #sort_models()
+  load('av_state.RData')
+  rejected_models <- sort_models(av_state$rejected_models)
+  expect_equal(length(rejected_models),length(av_state$rejected_models))
+  prev <- 0
+  for (model in rejected_models) {
+    expect_true(prev <= model_score(model$varest))
+    prev <- model_score(model$varest)
+  }
 })
