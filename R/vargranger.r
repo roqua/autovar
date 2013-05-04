de740508faf0a14d7a61db6393ba7c5d69d96fb8
Ando Emerencia <ando.emerencia@gmail.com>
2013-05-04 15:06:37 +0200

    Initial commit for compact vargranger list.

--- R/vargranger.r
+++ R/vargranger.r
@@ -378,6 +378,10 @@ vargranger_list <- function(lst) {
   df
 }
 
+compact_vargranger_list <- function(df) {
+  
+}
+
 format_as_percentage <- function(frac) {
   paste(format(round(100*frac,digits=2),nsmall=2,width=6),"%",sep='')
 }
