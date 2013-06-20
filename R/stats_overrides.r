7f68f71d8813c865bdf4470ec6654f3d787c967c
Ando Emerencia <ando.emerencia@gmail.com>
2013-06-20 11:51:24 +0200

    summary.lm rewrite to use pnorm instead of pt. Makes little to no difference in results, so leave it unused for now.

--- R/stats_overrides.r
+++ R/stats_overrides.r
@@ -106,10 +106,107 @@ order_coefficients <- function(mtx) {
   mtx[sorted_all,]
 }
 
+mysummary.lm <- function (object, correlation = FALSE, symbolic.cor = FALSE, 
+                        ...) 
+{
+  z <- object
+  p <- z$rank
+  rdf <- z$df.residual
+  if (p == 0) {
+    r <- z$residuals
+    n <- length(r)
+    w <- z$weights
+    if (is.null(w)) {
+      rss <- sum(r^2)
+    }
+    else {
+      rss <- sum(w * r^2)
+      r <- sqrt(w) * r
+    }
+    resvar <- rss/rdf
+    ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
+    class(ans) <- "summary.lm"
+    ans$aliased <- is.na(coef(object))
+    ans$residuals <- r
+    ans$df <- c(0L, n, length(ans$aliased))
+    ans$coefficients <- matrix(NA, 0L, 4L)
+    dimnames(ans$coefficients) <- list(NULL, c("Estimate", 
+                                               "Std. Error", "t value", "Pr(>|t|)"))
+    ans$sigma <- sqrt(resvar)
+    ans$r.squared <- ans$adj.r.squared <- 0
+    return(ans)
+  }
+  if (is.null(z$terms)) 
+    stop("invalid 'lm' object:  no 'terms' component")
+  if (!inherits(object, "lm")) 
+    warning("calling summary.lm(<fake-lm-object>) ...")
+  Qr <- stats:::qr.lm(object)
+  n <- NROW(Qr$qr)
+  if (is.na(z$df.residual) || n - p != z$df.residual) 
+    warning("residual degrees of freedom in object suggest this is not an \"lm\" fit")
+  p1 <- 1L:p
+  r <- z$residuals
+  f <- z$fitted.values
+  w <- z$weights
+  if (is.null(w)) {
+    mss <- if (attr(z$terms, "intercept")) 
+      sum((f - mean(f))^2)
+    else sum(f^2)
+    rss <- sum(r^2)
+  }
+  else {
+    mss <- if (attr(z$terms, "intercept")) {
+      m <- sum(w * f/sum(w))
+      sum(w * (f - m)^2)
+    }
+    else sum(w * f^2)
+    rss <- sum(w * r^2)
+    r <- sqrt(w) * r
+  }
+  resvar <- rss/rdf
+  R <- chol2inv(Qr$qr[p1, p1, drop = FALSE])
+  se <- sqrt(diag(R) * resvar)
+  est <- z$coefficients[Qr$pivot[p1]]
+  tval <- est/se
+  ans <- z[c("call", "terms", if (!is.null(z$weights)) "weights")]
+  ans$residuals <- r
+  ans$coefficients <- cbind(est, se, tval, 2 * pnorm(abs(tval), lower.tail = FALSE))
+  dimnames(ans$coefficients) <- list(names(z$coefficients)[Qr$pivot[p1]], 
+                                     c("Estimate", "Std. Error", "t value", "Pr(>|z|)"))
+  ans$aliased <- is.na(coef(object))
+  ans$sigma <- sqrt(resvar)
+  ans$df <- c(p, rdf, NCOL(Qr$qr))
+  if (p != attr(z$terms, "intercept")) {
+    df.int <- if (attr(z$terms, "intercept")) 
+      1L
+    else 0L
+    ans$r.squared <- mss/(mss + rss)
+    ans$adj.r.squared <- 1 - (1 - ans$r.squared) * ((n - 
+                                                       df.int)/rdf)
+    ans$fstatistic <- c(value = (mss/(p - df.int))/resvar, 
+                        numdf = p - df.int, dendf = rdf)
+  }
+  else ans$r.squared <- ans$adj.r.squared <- 0
+  ans$cov.unscaled <- R
+  dimnames(ans$cov.unscaled) <- dimnames(ans$coefficients)[c(1, 
+                                                             1)]
+  if (correlation) {
+    ans$correlation <- (R * resvar)/outer(se, se)
+    dimnames(ans$correlation) <- dimnames(ans$cov.unscaled)
+    ans$symbolic.cor <- symbolic.cor
+  }
+  if (!is.null(z$na.action)) 
+    ans$na.action <- z$na.action
+  class(ans) <- "summary.lm"
+  ans
+}
 
 # overriding the function
 .onLoad <- function(libname,pkgname) {
   override_function("printCoefmat","stats",myprintCoefmat)
+  # uncomment the line below to have coefficients that 
+  # correspond better to those calculated in stata
+  # override_function("summary.lm","stats",mysummary.lm)
 }
 
 override_function <- function(origfuncname,packagename,newfunc) {
