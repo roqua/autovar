85d3efa642c96c1b54612d8a8c0b714a573abfd4
Ando Emerencia <ando.emerencia@gmail.com>
2018-07-10 14:38:57 +0200

    use 3 cores

--- R/generate_networks.r
+++ R/generate_networks.r
@@ -84,7 +84,7 @@ generate_networks <- function(data, timestamp, always_include = NULL, pairs = NU
   if (!is.null(check_res)) return(check_res)
   #mycores <- parallel::detectCores()
   #if (is.na(mycores))
-  mycores <- 2
+  mycores <- 3
   for (attempt in 1:(net_cfg$max_network_size)) {
     fail_safe <- FALSE
     number_of_columns <- net_cfg$max_network_size
