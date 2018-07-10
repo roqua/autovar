664c2024b2e7aff3aa1a35f2be6bf932dae5a4f5
Ando Emerencia <ando.emerencia@gmail.com>
2018-07-10 15:51:38 +0200

    also install covr package

--- inst/bash/install-package-dependencies.sh
+++ inst/bash/install-package-dependencies.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env Rscript
 
 options(repos=structure(c(CRAN="http://cran.uni-muenster.de/")))
-packages <- c("Amelia", "e1071", "foreign", "ggplot2", "gridExtra", "igraph", "jsonlite", "knitr", "markdown", "norm", "parallel", "psych", "RcppArmadillo", "reshape2", "stringi", "stringr", "TimeProjection", "urca", "vars", "roxygen2", "testthat");
+packages <- c("Amelia", "e1071", "foreign", "ggplot2", "gridExtra", "igraph", "jsonlite", "knitr", "markdown", "norm", "parallel", "psych", "RcppArmadillo", "reshape2", "stringi", "stringr", "TimeProjection", "urca", "vars", "roxygen2", "testthat", "covr");
 new.packages <- packages[!(packages %in% installed.packages()[,"Package"])];
 if(length(new.packages))
   install.packages(new.packages);
