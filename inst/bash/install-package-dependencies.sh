af9abce0686bc498243ba1063837a3a30ead9563
Ando Emerencia <ando.emerencia@gmail.com>
2018-07-10 15:41:23 +0200

    change cran mirror

--- inst/bash/install-package-dependencies.sh
+++ inst/bash/install-package-dependencies.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env Rscript
 
-options(repos=structure(c(CRAN="http://cran-mirror.cs.uu.nl/")))
+options(repos=structure(c(CRAN="http://cran.uni-muenster.de/")))
 packages <- c("Amelia", "e1071", "foreign", "ggplot2", "gridExtra", "igraph", "jsonlite", "knitr", "markdown", "norm", "parallel", "psych", "RcppArmadillo", "reshape2", "stringi", "stringr", "TimeProjection", "urca", "vars", "roxygen2", "testthat");
 new.packages <- packages[!(packages %in% installed.packages()[,"Package"])];
 if(length(new.packages))
